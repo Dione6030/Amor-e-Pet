1e25dbc3d76cf2e16a34ba758d3909cb6e9ba2ad
Dione F.P. <dionepinheiro6030@gmail.com>
2025-11-19 19:50:52 -0300

    ✨ Adiciona componente Footer ao App

--- Amor e Pet/src/App.jsx
+++ Amor e Pet/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer'
 import Header from './components/Header'
 import './index.css'
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <Header />
+
+      <Footer />
     </>
   )
 }
