bdfdcc47ad1648c862a0f6468ea59aba031a95f4
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-20 15:55:11 -0300

    ✨ Adiciona a página Loja

---
+++ Amor e Pet/src/Loja.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+
+function Loja() {
+
+    return (
+        <>
+        <Header />
+
+        <main>
+            
+        </main>
+
+        <Footer />
+        </>
+    )
+}
+
+export default Loja
--- Amor e Pet/src/main.jsx
+++ Amor e Pet/src/main.jsx
@@ -5,10 +5,12 @@ import './index.css'
 
 import App from './App.jsx'
 import Login from './Login.jsx'
+import Loja from './Loja.jsx'
 
 const router = createBrowserRouter([
   { path: "/", element: <App/> },
-  { path: "/Login", element: <Login /> }
+  { path: "/Login", element: <Login /> },
+  { path: "/Loja", element: <Loja /> }
 ])
 
 createRoot(document.getElementById('root')).render(
