ab8512744467e60d73b530fa2b9a765f7f8c95d6
Dione <dionepinheiro6030@gmail.com>
2025-11-21 19:30:09 -0300

    ✨ Adiciona funcionalidade para listar produtos

--- Amor e Pet/src/AtualizarLoja.jsx
+++ Amor e Pet/src/AtualizarLoja.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Link } from "react-router-dom";
+import CardAtualizar from "./components/CardAtualizar";
 
 
 function AtualizarLoja() {
-    
+    const [ produtos, setProdutos ] = useState([])
+
+    useEffect(() => {
+        async function buscarProdutos() {
+            try {
+                const resposta = await fetch("http://localhost:3000/produtos")
+                if (!resposta.ok) throw new Error("Erro ao consultar os produtos")
+                const dados = await resposta.json()
+                setProdutos(dados)
+            } catch (error) {
+                console.log("Erro: ", error.message)
+            }
+        }
+        buscarProdutos();
+    }, []);
+
+    const listarProdutos = produtos.map((produto) => (
+        <CardAtualizar key={produto.id} produto={produto} setProdutos={setProdutos} />
+    ))
+
     return (
         <>
             <Header />
@@ -14,6 +35,10 @@ function AtualizarLoja() {
                 <div className="flex flex-col md:flex-row justify-around items-center gap-10 md:gap-20">
                     <button className="border border-white rounded-lg bg-a-escuro px-3 py-2 font-text text-3xl text-a-agua text-outline-3 drop-shadow-xl/50 cursor-pointer w-50">Adicionar produtos</button>
                     <button className="border border-white rounded-lg bg-a-escuro px-3 py-2 font-text text-3xl text-a-agua text-outline-3 drop-shadow-xl/50 cursor-pointer w-50">Pesquisar produtos</button>
+                </div>
+
+                <div className='bg-a-agua flex flex-wrap justify-center gap-8 p-5 w-full md:max-w-4xl max-h-150 xl:max-h-160 xl:h-160 overflow-auto rounded-lg'>
+                    {listarProdutos}
                 </div>
 
             </main>
