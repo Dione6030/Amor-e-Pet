70d79a3e9652711d01159b85e0b6cbe5dc83331a
Dione <dionepinheiro6030@gmail.com>
2025-11-21 19:24:29 -0300

    ✨ Adiciona componente CardAtualizar

---
+++ Amor e Pet/src/components/CardAtualizar.jsx
@@ -0,0 +1,17 @@
+
+
+export default function CardAtualizar({ produto }) {
+
+    return (
+        <div className="flex flex-col items-center gap-4 rounded-sm bg-a-claro w-60 md:w-65 pb-4">
+            <img src={produto.img} alt="Imagem do produto" className="w-full md:h-80 rounded-t-sm" />
+            <div className="flex flex-col items-start gap-2 px-4">
+                <h3 className="text-3xl font-text text-a-agua text-outline-2">{produto.nome}</h3>
+                <p className="text-xl font-text text-a-agua text-outline-2">{produto.categorias}</p>
+                <p className="text-xl font-text text-a-agua text-outline-2">R$ {produto.preco}</p>
+            </div>
+            <button className="border border-white rounded-lg bg-a-escuro px-3 py-2 font-text text-xs md:text-xl text-a-agua text-outline-3 hover:bg-a-escuro/90 active:scale-95 transition">Atualizar</button>
+            <button className="border border-white rounded-lg bg-red-600 px-3 py-2 font-text text-xs md:text-xl text-a-agua text-outline-3 hover:bg-red-600/90 active:scale-95 transition">Remover</button>
+        </div>
+    )
+}
