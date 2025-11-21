c7f51014b44e0cad88a441d8c1ce2f6419ddb829
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-21 18:02:09 -0300

    ✨ Adiciona AtualizarLoja

---
+++ Amor e Pet/src/AtualizarLoja.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+function AtualizarLoja() {
+    
+    return (
+        <>
+            <Header />
+
+            <main></main>
+
+            <Footer />
+        </>
+    )
+}
+
+export default AtualizarLoja;
